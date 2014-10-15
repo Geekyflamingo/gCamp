0817fffe36868495f98c3d8766f7c43d57d5e437
Becca <rebecca.vasil@gmail.com>
2014-10-15 13:40:10 -0600

    refactored quotes into controller

--- app/controllers/concerns/pages_controller.rb
+++ app/controllers/concerns/pages_controller.rb
@@ -1,3 +1,9 @@
 class PagesController <ApplicationController
 
+  def index
+    @quotes=[["People who think they know everything are a great annoyance to those of us who do.", "Issac Asimov"],
+            ["If you could kick the person in the pants responsible for most of your trouble, you wouldn't sit for a month.", "Theodore Roosevelt"],
+            ["Get your facts first, then you can distort them as you please.","Mark Twain"]]
+  end
+
 end
