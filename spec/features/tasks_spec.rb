e62e502ddf26fac036b4a56c27b07f4dae1b8f25
Becca <rebecca.vasil@gmail.com>
2014-11-04 18:03:57 -0700

    added tests for show, edit and destroy for tasks

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -16,60 +16,61 @@ feature "Tasks" do
     click_on "Create Task"
 
     expect(page).to have_content("Extreme Testing!")
-    save_and_open_page
+
 
   end
 
-  # scenario "User edits a user" do
-  #
-  #   User.create!(
-  #     first_name: "Mr. T" , last_name: "Pity the fool!" , email: "mrt@example.com",
-  #     password: "pass", password_confirmation: "pass"
-  #   )
-  #
-  #   visit users_path
-  #   expect(page).to have_content("Mr. T")
-  #   click_on "Edit"
-  #   fill_in "First Name", with: "Frank"
-  #   fill_in "Last Name", with: "Sinatra"
-  #   fill_in "Email", with: "blueeyes@example.com"
-  #   click_on "Update User"
-  #
-  #   expect(page).to have_content("Frank")
-  #   expect(page).to have_no_content("Mr. T")
-  #
-  # end
-  #
-  # scenario "User wants to see a user" do
-  #
-  #   User.create!(
-  #     first_name: "Mr. T" , last_name: "Pity the fool!" , email: "mrt@example.com",
-  #     password: "pass", password_confirmation: "pass"
-  #   )
-  #
-  #   visit users_path
-  #   expect(page).to have_content("Mr. T")
-  #   click_on "Mr. T"
-  #   expect(page).to have_content("Mr. T")
-  #   expect(page).to have_no_content("Password")
-  #   save_and_open_page
-  #
-  # end
-  #
-  # scenario "User wants to delete a user" do
-  #
-  #   User.create!(
-  #     first_name: "Mr. T" , last_name: "Pity the fool!" , email: "mrt@example.com",
-  #     password: "pass", password_confirmation: "pass"
-  #   )
-  #
-  #   visit users_path
-  #   expect(page).to have_content("Mr. T")
-  #   click_on "Edit"
-  #   click_on "Delete User"
-  #   expect(page).to have_no_content("Mr. T")
-  #   save_and_open_page
-  #
-  # end
+  scenario "User wants to see a task" do
+
+    Task.create!(
+      description: "Feed the dog" , complete: "false" , due_date: "11/04/2014"
+    )
+
+      visit tasks_path
+      expect(page).to have_content("Feed the dog")
+      click_on "Show"
+      expect(page).to have_content("Feed the dog")
+      expect(page).to have_no_content("Password")
+      save_and_open_page
+
+  end
+
+  scenario "User wants to edit a task" do
+
+    Task.create!(
+      description: "Feed the dog" , complete: "false" , due_date: "11/04/2014"
+    )
+
+      visit tasks_path
+      expect(page).to have_content("Feed the dog")
+      click_on "Edit"
+      expect(page).to have_content("Editing task")
+      expect(page).to have_no_content("Password")
+      check "Complete"
+      click_on "Update Task"
+      expect(page).to have_content("Feed the dog")
+
+      click_on "Tasks"
+      expect(page).to have_no_content("Feed the dog")
+
+      click_on "All"
+      expect(page).to have_content("Feed the dog")
+      save_and_open_page
+
+  end
+
+  scenario "User wants to delete a Task" do
+
+    Task.create!(
+      description: "Feed the dog" , complete: "false" , due_date: "11/04/2014"
+    )
+
+    visit tasks_path
+    expect(page).to have_content("Feed the dog")
+    click_on "Destroy"
+    expect(page).to have_no_content("Feed the dog")
+    save_and_open_page
+
+  end
 
 end
