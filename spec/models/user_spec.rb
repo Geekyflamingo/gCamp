c9d33636cf0a55e2b1599797da77a2c1ba2631e8
Becca <rebecca.vasil@gmail.com>
2014-11-12 20:00:10 -0700

    added model user spec for email uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 describe User do
 
-  it 'verifies that user is invaild' do
+  it 'validates that user is invaild' do
 
     user = User.new
     expect(user.valid?).to be(false)
@@ -16,7 +16,7 @@ describe User do
 
   end
 
-  it 'verifies that user is vaild' do
+  it 'validates that user is vaild' do
 
     user = User.new
     expect(user.valid?).to be(false)
@@ -29,6 +29,22 @@ describe User do
     user.password='pass'
     expect(user.valid?).to be(true)
 
+  end
+
+  it 'validates that user email is unique regardless of case' do
+
+    User.create!(first_name: "roger", last_name: "smith", email: "roger@example.com", password: "pass")
+
+    User.new(email: "roger@example.com")
+    user = User.new
+    user.valid?
+    expect(user.errors[:email].present?).to eq(true)
+    user.email = "Roger@example.com"
+    user.valid?
+    expect(user.errors[:email].present?).to eq(true)
+    user.email = "aa@example.com"
+    user.valid?
+    expect(user.errors[:email].present?).to eq(false)
   end
 
 end
