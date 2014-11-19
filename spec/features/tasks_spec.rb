9aeac060ad16455807bb57700334c3a8be0e3870
Becca <rebecca.vasil@gmail.com>
2014-11-19 13:47:59 -0700

    fixed format for membership page

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -26,43 +26,26 @@ feature "Tasks" do
 
   end
 
-  # scenario "User wants to see a task" do
-  #   Task.create!(
-  #     description: "Feed the dog" , complete: "false" , due_date: Date.today
-  #   )
-  #
-  #   visit projects_path
-  #   expect(page).to have_content("Projects")
-  #   click_on "YAY!"
-  #   expect(page).to have_content("Edit")
-  #   save_and_open_page
-  #   click_link "1 Task"
-  #   click_on "Show"
-  #   expect(page).to have_content("Feed the dog")
-  #   expect(page).to have_no_content("Task was successfully created.")
-  #
-  # end
+  scenario "User wants to edit a task" do
+  
+    Task.create!(
+      description: "Feed the dog" , complete: "false" , due_date: Date.today
+    )
 
-  # scenario "User wants to edit a task" do
-  #
-  #   Task.create!(
-  #     description: "Feed the dog" , complete: "false" , due_date: Date.today
-  #   )
-  #
-  #   visit tasks_path
-  #   expect(page).to have_content("Feed the dog")
-  #   click_on "Edit"
-  #   expect(page).to have_content("Editing task")
-  #   expect(page).to have_no_content("Password")
-  #   check "Complete"
-  #   click_on "Update Task"
-  #   expect(page).to have_content("Feed the dog")
-  #   click_on "Tasks"
-  #   expect(page).to have_no_content("Feed the dog")
-  #   click_on "All"
-  #   expect(page).to have_content("Feed the dog")
-  #
-  # end
+    visit tasks_path
+    expect(page).to have_content("Feed the dog")
+    click_on "Edit"
+    expect(page).to have_content("Editing task")
+    expect(page).to have_no_content("Password")
+    check "Complete"
+    click_on "Update Task"
+    expect(page).to have_content("Feed the dog")
+    click_on "Tasks"
+    expect(page).to have_no_content("Feed the dog")
+    click_on "All"
+    expect(page).to have_content("Feed the dog")
+
+  end
   #
   # scenario "User wants to delete a Task" do
   #
