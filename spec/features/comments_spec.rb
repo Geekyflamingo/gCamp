9d6dd5c523c94efb9c26f5b64766c6b98d1379eb
Becca <rebecca.vasil@gmail.com>
2014-11-21 15:08:27 -0700

    test written for comments

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -16,11 +16,23 @@ feature "Comments" do
       email: "do@example.com",
         password: "pass", password_confirmation: "pass"
       )
+    visit root_path
+    expect(page).to have_content("gCamp")
+    click_link ("Sign In")
+    expect(page).to have_content("Sign into gCamp")
+    fill_in "Email", with: "do@example.com"
+    fill_in "Password", with: "pass"
+    click_button("Sign in")
+    expect(page).to have_content("Dodger Oliver")
+    expect(page).to have_content("Sign Out")
+    expect(page).to have_no_content("Sign In")
 
     visit project_tasks_path(project)
     click_on "Feed the dog"
     fill_in  "comment_description", with: "My comment"
     click_on "Add Comment"
     expect(page).to have_content("My comment")
+
+
   end
 end
