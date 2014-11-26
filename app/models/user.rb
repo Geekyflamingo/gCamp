2946ab7e051098191df1669abe43329f95d65893
Becca <rebecca.vasil@gmail.com>
2014-11-26 08:32:31 -0700

    tasks show page now shows deleted user when a user is deleted.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :memberships, dependent: :destroy
   has_many :projects, through: :memberships
-  has_many :comments
+  has_many :comments, dependent: :nullify
 
   validates :first_name, presence: true
   validates :last_name, presence: true
